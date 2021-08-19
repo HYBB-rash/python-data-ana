35ef5bacf72426a155cd657211e76600989cab3b
hyong <hyong_cs@outlook.com>
2021-08-19 22:24:54 +0800

    可视化

---
+++ ch9_pic_visual.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   ch9_pic_visual.py
+@Time    :   2021/07/30 14:21:06
+@Author  :   hyong 
+@Version :   1.0
+@Contact :   hyong_cs@outlook.com
+'''
+# here put the import lib
+
+# %%
+
+import matplotlib.pyplot as plt
+%matplotlib notebook
+
+# %%
+
+import numpy as np
+data = np.arange(10)
+data
+
+# %%
+
+plt.plot(data)
+
+# %%
